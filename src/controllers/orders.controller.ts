e3fe8e26f8f0f7da46de96f8ea540582da0cab86
Nagireddi Sumanth <sumanthnagireddi@gmail.com>
2025-05-10 11:43:41 +0530

    added more apis on order module

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -10,9 +10,9 @@ export const getOrders = async (req: Request, res: Response, next: NextFunction)
     }
 }
 
-export const createOrder = async (req:Request,res:Response,next:NextFunction)=>{
+export const createOrder = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const order_payload=req.body
+        const order_payload = req.body
         const orders = await orderModel.create(order_payload)
         res.status(200).json(orders)
     } catch (error) {
@@ -20,23 +20,123 @@ export const createOrder = async (req:Request,res:Response,next:NextFunction)=>{
     }
 }
 
-export const getOrderById = async (req:Request,res:Response,next:NextFunction)=>{
+export const getOrderById = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const order_id=req.params.id;
-        const orders=await orderModel.findById(order_id)
+        const order_id = req.params.id;
+        const order = await orderModel.findById(order_id)
+        if (!order) {
+            res.status(404).json({ message: "Order not found.", data: {} });
+        }
+        res.status(200).json({ message: "Order Fetched Succesfully!!.", data: order })
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const getUserOrders = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const user_id = req.params.user_id;
+        const orders = await orderModel.find({ user: user_id })
         res.status(200).json(orders)
     } catch (error) {
         next(error)
     }
 }
 
-export const getUserOrders = async (req:Request,res:Response,next:NextFunction)=>{
+export const deleteOrder = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const user_id=req.params.user_id;
-        console.log(user_id)
-        const orders= await orderModel.find({user:user_id})
-        res.status(200).json(orders)
+        const order_id = req.params.id;
+        const order = await orderModel.findByIdAndDelete(order_id);
+        res.status(200).json({ message: "Order Deleted Succesfully!!!" })
     } catch (error) {
         next(error)
     }
 }
+
+export const updateOrderDetails = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const order_id = req.params.id;
+        const order_payload = req.body;
+
+        if (!order_id) {
+            res.status(400).json({ message: "Order ID is required." });
+        }
+
+        const updatedOrder = await orderModel.findByIdAndUpdate(order_id, order_payload, { new: true });
+
+        if (!updatedOrder) {
+            res.status(404).json({ message: "Order not found." });
+        }
+
+        res.status(200).json({
+            message: "Order updated successfully!",
+            data: updatedOrder,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+// payment
+
+export const getPaymentStatus = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const order_id = req.params.order_id;
+
+        if (!order_id) {
+            res.status(400).json({ message: "Order ID is required." });
+        }
+
+        const order = await orderModel.findById( order_id );
+
+        if (!order) {
+            res.status(404).json({ message: "Order not found." });
+        }
+
+        res.status(200).json({
+            message: "Payment status fetched successfully!",
+            data: {
+                order_id: order?._id,
+                paymentStatus: order?.paymentResult,
+            },
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+// order status
+
+export const updateOrderStatus = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const order_id = req.params.id;
+        const { orderStatus } = req.body;
+
+        if (!order_id) {
+             res.status(400).json({ message: "Order ID is required." });
+        }
+
+        if (!orderStatus) {
+             res.status(400).json({ message: "Status is required to update the order." });
+        }
+
+        const updatedOrder = await orderModel.findByIdAndUpdate(
+            order_id,
+            { orderStatus },
+            { new: true }
+        );
+
+        if (!updatedOrder) {
+             res.status(404).json({ message: "Order not found." });
+        }
+
+        res.status(200).json({
+            message: "Order status updated successfully!",
+            data: updatedOrder,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
--- src/routes/order.route.ts
+++ src/routes/order.route.ts
@@ -1,10 +1,24 @@
 import express from "express";
 import { userAuth } from "../middleware/userAuth";
-import { getOrders, createOrder, getOrderById, getUserOrders } from "../controllers/orders.controller";
+import {
+  getOrders,
+  createOrder,
+  getOrderById,
+  getUserOrders,
+  deleteOrder,
+  updateOrderDetails,
+  getPaymentStatus,
+  updateOrderStatus
+} from "../controllers/orders.controller";
+
 const orders_router = express.Router();
 
+orders_router.get("/users/:user_id", getUserOrders); 
 orders_router.get("/", getOrders);
-orders_router.post("/", createOrder)
-orders_router.get("/:id", getOrderById)
-orders_router.get("/users/:user_id", getUserOrders)
-export default orders_router
+orders_router.post("/", createOrder);
+orders_router.get("/:id", getOrderById); 
+orders_router.delete("/:id",deleteOrder)
+orders_router.patch("/:id",updateOrderDetails);
+orders_router.get("/payment_status/:order_id",getPaymentStatus)
+orders_router.patch("/update_order_status/:id",updateOrderStatus)
+export default orders_router;
